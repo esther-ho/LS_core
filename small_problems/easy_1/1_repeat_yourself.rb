c42c7a82c51080aac91fb540c48c0c85e3be5c5c
Esther Ho <ekyho@ucdavis.edu>
2022-10-27 14:50:37 -0700

    Add solution to Easy 1 - Exercise 1

---
+++ small_problems/easy_1/1_repeat_yourself.rb
@@ -0,0 +1,40 @@
+=begin
+Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates
+
+# repeat('Hello', 3) # => Prints 'Hello' on a new line 3 times
+
+----- P
+Inputs: A single string, a positive whole number
+
+Output: The given string printed to the screen 'integer'-number of times
+
+Implicit rules:
+- The string is printed on a new line each time
+- The method does not mutate the input
+- The return value is not important.
+
+----- E
+repeat('Hello', 3) => Prints 'Hello' on a new line 3 times
+repeat('Hello world', 5) => Prints 'Hello world' on a new line 5 times
+
+----- D
+
+----- A
+- Set a counter to count the iterations as 1
+- While the counter is less than or equal to the given positive number,
+  - Print the given string
+  - Increment the counter by 1
+  
+=end
+
+def repeat(string, number)
+  counter = 1
+
+  while counter <= number
+    puts string
+    counter += 1
+  end
+end
+
+repeat('Hello', 3)
+repeat('Hello world', 5)
