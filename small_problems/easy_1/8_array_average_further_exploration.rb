ecf0ad6610933bd5ee561af473b77ace274f81ca
Esther Ho <ekyho@ucdavis.edu>
2022-11-02 16:44:41 -0700

    Edit examples portion of the further explorations solution to Easy 1 - Exercise 8

--- small_problems/easy_1/8_array_average_further_exploration.rb
+++ small_problems/easy_1/8_array_average_further_exploration.rb
@@ -13,9 +13,9 @@ Implicit rules:
 - The output will be a decimal point number greater than 0
 
 --- E
-puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
-puts average([1, 5, 87, 45, 8, 8]) == 25
-puts average([9, 47, 23, 95, 16, 52]) == 40
+puts average([1, 6]) == 3.5 # float division: (1 + 6) / 2 -> 3.5
+puts average([1, 5, 87, 45, 8, 8]) == 25.67
+puts average([9, 47, 23, 95, 16, 52]) == 40.34
 
 --- D
 The input is provided as an array of whole numbers
