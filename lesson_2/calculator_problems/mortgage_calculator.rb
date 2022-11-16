a65f43642ecacb331b685b7b7640b51c9c5d29f6
Esther Ho <ekyho@ucdavis.edu>
2022-11-16 11:56:50 -0800

    Add 'message' method to load prompts from YAML file

---
+++ lesson_2/calculator_problems/mortgage_calculator.rb
@@ -0,0 +1,56 @@
+=begin
+--- P
+Inputs:
+- Loan amount e.g. how much is being borrowed
+- Annual percentage rate, the percent of interest paid yearly based on the loan amount
+- Duration of loan
+  - Allow user to choose to enter duration in years or months
+
+Outputs:
+- Interest rate per month
+- Duration of the loan in months (e.g. how many total payments)
+- Total amount to be paid per month
+
+Implicit rules:
+- APR will be given in % e.g. 5%
+- Calculator is for a fixed rate mortgage
+- No upfront payments
+- Tax is not included in the payments
+
+--- E
+
+--- D
+
+--- A
+- Ask user for their name
+  - Check if name is valid
+- Ask user for their total loan amount
+  - Check if loan amount is valid
+- Ask user for their APY in % (e.g. 5%)
+  - Check if APY is valid
+- Ask user if they would like to input duration in years or months
+  - Check if reply is valid
+  - Check if duration is valid
+- Calculate interest rate per month
+  - Round to nearest two decimal places
+- Calculate duration of loan in months (total payments)
+  - Round to nearest integer
+- Calculate total amount to be paid per month
+  - Round to nearest two decimal places
+- Return all calculations to the user
+- Ask user if they would like to make changes or restart
+  - Check if answer is valid
+=end
+
+require 'psych'
+
+MESSAGES = Psych.load_file('mortgage_config.yml')
+LANGUAGE = 'en'
+
+# Retrieve the appropriate prompt based on the language
+def message(key, lang=LANGUAGE)
+  MESSAGES[lang][key]
+end
+
+
+
