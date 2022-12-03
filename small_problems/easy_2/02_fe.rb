f3da458db1ab7fbc9c0bb0c05a592b4e41b58a7d
Esther Ho <ekyho@ucdavis.edu>
2022-12-03 15:44:01 -0800

    Add code for Exercise 2 - Further Exploration

--- small_problems/easy_2/02_fe.rb
+++ small_problems/easy_2/02_fe.rb
@@ -23,3 +23,21 @@ Hash to store values to convert square feet to:
 - Convert square feet to square inches and square cm
 - Print the results
 =end
+
+CONVERT_SQFEET = {
+  sqinch: 144,
+  sqcm: 929.03
+}
+
+puts "Enter the length of the room in feet:"
+length = gets.chomp.to_f
+
+puts "Enter the width of the room in feet:"
+width = gets.chomp.to_f
+
+sqfeet = length * width.round(2)
+sqinch = sqfeet * CONVERT_SQFEET[:sqinch].round(2)
+sqcm = sqfeet * CONVERT_SQFEET[:sqcm].round(2)
+
+puts "The area of the room is #{sqfeet} square feet.\n" \
+     "(#{sqinch} square inches / #{sqcm} square centimeters)"
