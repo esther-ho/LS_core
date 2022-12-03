5db243d11bbfb51fdb9efff14e720d156dd7850c
Esther Ho <ekyho@ucdavis.edu>
2022-12-03 15:34:04 -0800

    Add PEDAC for Exercise 2 - Further exploration

---
+++ small_problems/easy_2/02_fe.rb
@@ -0,0 +1,25 @@
+=begin
+--- P
+Inputs:
+- Length of room in feet
+- Width of room in feet
+Output:
+- Area of the room in:
+  - Square feet
+  - Square inches
+  - Square centimeters
+--- E
+
+--- D
+Hash to store values to convert square feet to:
+  - Square inches
+  - Square centimeters
+--- A
+- Initialize hash to store values to convert square feet
+- Ask user for length in feet and store the value
+- Ask user for width in feet and store the value
+- Convert the string input to floats
+- Multiply the values to get the area in square feet
+- Convert square feet to square inches and square cm
+- Print the results
+=end
