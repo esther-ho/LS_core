49e39bb463e2f4962126dcb13ea12f314800dc7a
Esther Ho <ekyho@ucdavis.edu>
2022-12-05 15:01:26 -0800

    Add PEDAC for Exercise 6

---
+++ small_problems/easy_2/06.rb
@@ -0,0 +1,19 @@
+=begin
+--- P
+Inputs:
+
+Outputs:
+- Print all odd numbers from 1 to 99, including 1 and 99
+- Each number is on a separate line
+Implcit rules:
+- Numbers are all whole numbers
+- Start printing from 1 to 99, in order
+--- E
+
+--- D
+Array of odd numbers present from 1 to 99
+--- A
+- Create an array with a range of numbers from 1 to 99
+- Filter the array to only contain odd numbers
+- Print the numbers in the array on a separate line each
+=end
