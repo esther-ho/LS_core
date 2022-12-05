3d16553a94d698654d604a55fd28250ec9697cf5
Esther Ho <ekyho@ucdavis.edu>
2022-12-05 14:57:10 -0800

    Mutate name if it ends with ''

--- small_problems/easy_2/05.rb
+++ small_problems/easy_2/05.rb
@@ -26,7 +26,7 @@ print "What is your name? "
 name = gets.chomp
 
 if name[-1] == '!'
-  name = name.chop
+  name.chomp!('!') # OR name.chop!
   puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
 else
   puts "Hello #{name}."
