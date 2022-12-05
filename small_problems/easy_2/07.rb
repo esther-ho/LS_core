cf976ad960e012b6f9a0c7171230d83f56e3efc5
Esther Ho <ekyho@ucdavis.edu>
2022-12-05 15:33:32 -0800

    Add PEDAC for Exercise 7

---
+++ small_problems/easy_2/07.rb
@@ -0,0 +1,19 @@
+=begin
+--- P
+Inputs:
+
+Outputs:
+- Print all even numbers from 1 to 99 inclusive
+- Print each number on a new line
+Implicit rules:
+- Numbers are all whole numbers
+- Numbers are printed in order from smallest to largest
+--- E
+
+--- D
+
+--- A
+- Initialize a range of numbers from 1 to 99
+- Select all even numbers from the range
+- Print the selected numbers
+=end
