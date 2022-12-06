1a8d751e98f893e8a2215b8b337e9d41c254f07f
Esther Ho <ekyho@ucdavis.edu>
2022-12-05 16:27:21 -0800

    Add explanation for code result

---
+++ small_problems/easy_2/09.rb
@@ -0,0 +1,11 @@
+=begin
+#upcase! is a mutating method.
+Therefore, `name.upcase!` mutates 'Bob' to 'BOB'.
+`save_name` points to the same object as `name`.
+Therefore, `puts name, save_name` will print 'BOB' twice
+=end
+
+name = 'Bob'
+save_name = name
+name.upcase!
+puts name, save_name
