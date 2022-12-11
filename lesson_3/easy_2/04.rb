73c0eb75731054083991e1d03e57da343a9eea52
Esther Ho <ekyho@ucdavis.edu>
2022-12-11 15:50:22 -0800

    Edit Exercise 4

--- lesson_3/easy_2/04.rb
+++ lesson_3/easy_2/04.rb
@@ -2,3 +2,5 @@ advice = "Few things in life are as important as house training your pet dinosau
 
 # Check if the name 'Dino' appears in the string
 advice.include?('Dino')
+# Alternative: `advice.match?('Dino')`
+# `#match?` is not perfect as it will match any substring with 'Dino' in it
