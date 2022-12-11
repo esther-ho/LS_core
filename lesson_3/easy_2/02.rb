11a78db3a4ec6c85f6ba7d2ccaa40291be1de641
Esther Ho <ekyho@ucdavis.edu>
2022-12-10 16:32:50 -0800

    Add Exercise 2

---
+++ lesson_3/easy_2/02.rb
@@ -0,0 +1,14 @@
+=begin
+Convert the string into:
+- All uppercase except for 1st letter of 1st and 2nd word
+- Capitalize only the first word
+- All lowercase
+- All uppercase
+=end
+
+munsters_description = "The Munsters are creepy in a good way."
+
+puts munsters_description.swapcase
+puts munsters_description.capitalize
+puts munsters_description.downcase
+puts munsters_description.upcase 
