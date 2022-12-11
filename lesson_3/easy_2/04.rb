006503a5c13e1df666e695e82bf29b0d067d93c4
Esther Ho <ekyho@ucdavis.edu>
2022-12-10 17:21:08 -0800

    Edit Exercise 4

--- lesson_3/easy_2/04.rb
+++ lesson_3/easy_2/04.rb
@@ -1,4 +1,4 @@
-advice = "Few things in life are as important as house training your pet dinosaur"
+advice = "Few things in life are as important as house training your pet dinosaur."
 
 # Check if the name 'Dino' appears in the string
 advice.include?('Dino')
