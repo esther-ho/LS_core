6c21bde12c4b14b578e6ce69c35f6967aa949ea9
Esther Ho <ekyho@ucdavis.edu>
2022-12-10 16:48:21 -0800

    Add Exercise 6

---
+++ lesson_3/easy_2/06.rb
@@ -0,0 +1,9 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# Add 'Dino' to the array
+flintstones << 'Dino'
+=begin
+Alternatives:
+- flintstones.push('Dino')
+- flintstones.append('Dino')
+- flintstones.concat(['Dino'])
