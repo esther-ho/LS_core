17e8f61f408d52b2acedb98cc436e76ddc087be8
Esther Ho <ekyho@ucdavis.edu>
2022-12-10 16:09:13 -0800

    Add Exercise 1

---
+++ lesson_3/easy_2/01.rb
@@ -0,0 +1,11 @@
+ages = {
+  "Herman" => 32,
+  "Lily" => 30,
+  "Grandpa" => 402,
+  "Eddie" => 10
+}
+
+# Check if 'Spot' is present
+ages.key?('Spot')
+ages.has_key?('Spot')
+ages.member?('Spot')
