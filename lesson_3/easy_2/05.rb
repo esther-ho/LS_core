9eae21104490be85d494a32fb452fbaae4bbf645
Esther Ho <ekyho@ucdavis.edu>
2022-12-10 16:44:39 -0800

    Add Exercise 5

---
+++ lesson_3/easy_2/05.rb
@@ -0,0 +1,11 @@
+flintstones = [
+  "Fred",
+  "Barney",
+  "Wilma",
+  "Betty",
+  "BamBam",
+  "Pebbles"
+]
+
+# Alternate way to write array
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
