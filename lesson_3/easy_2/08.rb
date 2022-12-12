515986ab35c3a5c26b206dd557d3d9213a4a75f0
Esther Ho <ekyho@ucdavis.edu>
2022-12-11 16:00:17 -0800

    Edit Exercise 8

--- lesson_3/easy_2/08.rb
+++ lesson_3/easy_2/08.rb
@@ -2,11 +2,11 @@ advice = "Few things in life are as important as house training your pet dinosau
 
 # Return "Few things in life are as imporant as "
 # Leave advice as "house training your pet dinosaur"
-advice.slice(0, 39)
-advice # Original string
+p advice.slice(0, advice.index('house')) # 'house' starts at index 39
+p advice # Original string
 
-advice.slice!(0, 39)
-advice # Mutated string
+p advice.slice!(0, advice.index('house'))
+p advice # Mutated string
 
 # Using `String#slice` will return the deleted portion
 # However, it will not mutate the original string
