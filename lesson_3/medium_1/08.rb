5a5e949a5eae1e62431eae07abf98ded82782de0
Esther Ho <ekyho@ucdavis.edu>
2022-12-12 20:14:38 -0800

    Add Exercise 8

---
+++ lesson_3/medium_1/08.rb
@@ -0,0 +1,7 @@
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+
+# rps("rock, "paper") #=> "paper"
+# rps("rock", "scissors") #=> "rock"
+# rps("paper", "rock") #=> "paper"
+# rps("paper", "rock") #=> "paper"
+# The final results is "paper".
