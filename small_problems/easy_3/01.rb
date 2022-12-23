7138af16302fe364fadae82cfbb8b94a0a6ab5a0
Esther Ho <ekyho@ucdavis.edu>
2022-12-23 15:30:21 -0800

    Add Exercise 1

--- small_problems/easy_3/01.rb
+++ small_problems/easy_3/01.rb
@@ -34,3 +34,46 @@ Implicit rules:
 - Else, print that it does not appear in the array
 
 =end
+
+def valid_number?(number)
+  /^[[:digit:]]+$/.match(number)
+end
+
+def count_message(number)
+  case number
+  when 1 then "1st"
+  when 2 then "2nd"
+  when 3 then "3rd"
+  when 4 then "4th"
+  when 5 then "5th"
+  end
+end
+
+def get_number
+  number = ''
+
+  loop do
+    number = gets.chomp
+    break if valid_number?(number)
+    puts "That is not a valid number. Try again."
+  end
+
+  number
+end
+
+numbers = []
+until numbers.size == 5
+  puts "==> Enter the #{count_message(numbers.size + 1)} number:"
+
+  number = get_number
+  numbers << number.to_i
+end
+
+puts "==> Enter the last number:"
+last_number = get_number
+
+if numbers.include?(last_number.to_i)
+  puts "The number #{last_number} appears in #{numbers}."
+else
+  puts "The number #{last_number} does not appear in #{numbers}."
+end
