32d2aab780095def6abfb0b04774e5f7560a030e
Esther Ho <ekyho@ucdavis.edu>
2022-12-22 17:35:45 -0800

    Edit Exercise 2

--- lesson_3/hard_1/02.rb
+++ lesson_3/hard_1/02.rb
@@ -4,6 +4,13 @@ The last line will output "{:a=>"hi there"}".
 Using `#<<` concatenates ' there' to the String object and mutates it.
 `greetings` references the hash containing the same String object.
 Therefore, the value of the k-v pair in the hash is modified.
+
+Modifying `informal_greeting` without modifying `greetings`:
+1. Initalize `informal_greeting` to reference a new object with the same value.
+- `informal_greeting = greetings[:a].clone`
+2. String concatenation which returns a new String object
+-`informal_greeting += ' there'`
+
 =end
 
 greetings = { a: 'hi' }
