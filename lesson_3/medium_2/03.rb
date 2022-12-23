b93196f1a5130f358d6aaaa73b1425f9d86c1ab4
Esther Ho <ekyho@ucdavis.edu>
2022-12-22 16:11:01 -0800

    Edit Exercise 3

--- lesson_3/medium_2/03.rb
+++ lesson_3/medium_2/03.rb
@@ -1,16 +1,21 @@
 =begin
+When a reference value is passed, the method initializes a new local variable.
+Both reference values are assigned to new local variables (string and array).
+
 The local variable `a_string_param` in the method is an alias for `my_string`.
 - They reference the same String object.
 In the method, `a_string_param` gets reassigned to a new String object.
-- `#+=` creates a new String and binds `a_string_param` to it
+- `#+=` creates a new String object and binds `a_string_param` to it.
 - The new String object is "pumpkinsrutabaga".
-The original String is not modified
+`a_string_param` and `my_string` point to difference objects.
+The original String is not modified.
 - `my_string` still references "pumpkins".
 
 The local variable `an_array_param` is an alias for `my_array`.
 - They reference the same Array object.
 In the method, `an_array_param` gets mutated using `#<<`.
-- `#<<` appends a new element to the array
+- `#<<` appends a new element to the array.
+`my_array_param` and `my_array` still point to the same object.
 The original Array is modified.
 - `my_array` references `["pumpkins", "rutabaga"]`.
 =end
