109412492e6bf3a6b1868d62ad7aeb8e7d1541bc
Esther Ho <ekyho@ucdavis.edu>
2022-12-22 17:32:44 -0800

    Edit Exercise 1

--- lesson_3/hard_1/01.rb
+++ lesson_3/hard_1/01.rb
@@ -3,6 +3,10 @@ It will raise an NameError as `greeting` is an undefined local variable.
 The expression evaluated by the if conditional statement is `false`.
 Therefore, the resulting code is not evaluated.
 `"hello world"` does not get assigned to `greeting`.
+
+Initializing a local variable within an `if` clause will initialize it to `nil`.
+This happens even if the `if` clause doesn't get executed.
+Therefore, `greeting` here is `nil`.
 =end
 
 if false
