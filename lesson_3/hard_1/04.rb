b61e5b1b7ea8b4724bea4e73666ec535acfcd41a
Esther Ho <ekyho@ucdavis.edu>
2022-12-23 14:47:13 -0800

    Edit Exercise 4

--- lesson_3/hard_1/04.rb
+++ lesson_3/hard_1/04.rb
@@ -20,10 +20,20 @@ end
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
   return false if dot_separated_words.size != 4
+  # alternative: `return false unless dot_separated_words.size == 4`
 
-  dot_separated_words.each do |word|
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
     return false unless is_an_ip_number?(word)
   end
+  # Original code doesn't return false for non-numeric component.
+  # It used break, and control falls to the `return true` statement.
+  # Use `return false` instead of `break`.
+
+  # Alternative:
+  # dot_separated_words.each do |word|
+  #   return false unless is_an_ip_number?(word)
+  # end
 
   true
 end
