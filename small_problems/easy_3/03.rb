d1fe5611c1aab1877b15c5270c406fe2d2140537
Esther Ho <ekyho@ucdavis.edu>
2022-12-25 15:45:15 -0800

    Add Exercise 3

--- small_problems/easy_3/03.rb
+++ small_problems/easy_3/03.rb
@@ -9,15 +9,21 @@ Implicit rules:
 - Input consists of only alphabetical letters and punctuation
 - Punctuation is counted as characters
 - Spaces are not counted as characters
+- No validation of input needed
 --- E
 Please write word or multiple words: walk, don't run
 There are 13 characters in "walk, don't run".
 --- D
-- Store characters in an array
+
 --- A
 - Print the prompt for the user to enter a word or phrase
 - Store the input
-- Split the input into an array of individual characters without spaces
-- Count the number of elements in the array
+- Delete all spaces
+- Find the length of the remaining string
 - Print the number of characters in the word or phrase
 =end
+
+print "Please write word or multiple words: "
+words = gets.chomp
+char_count = words.delete(' ').size
+puts "There are #{char_count} characters in \"#{words}\"."
