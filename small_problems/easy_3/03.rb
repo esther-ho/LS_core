e2f9bca230d3cdb4be19f1db9bf007cabc7fc0d9
Esther Ho <ekyho@ucdavis.edu>
2022-12-25 15:34:09 -0800

    Add PEDAC for Exercise 3

---
+++ small_problems/easy_3/03.rb
@@ -0,0 +1,23 @@
+=begin
+--- P
+Inputs:
+- User input of a word or a phrase
+Outputs:
+- Number of characters in the input
+
+Implicit rules:
+- Input consists of only alphabetical letters and punctuation
+- Punctuation is counted as characters
+- Spaces are not counted as characters
+--- E
+Please write word or multiple words: walk, don't run
+There are 13 characters in "walk, don't run".
+--- D
+- Store characters in an array
+--- A
+- Print the prompt for the user to enter a word or phrase
+- Store the input
+- Split the input into an array of individual characters without spaces
+- Count the number of elements in the array
+- Print the number of characters in the word or phrase
+=end
