b12cdcbe97b0eac781cb92c9dc4e77f94155b7d4
Esther Ho <ekyho@ucdavis.edu>
2022-12-25 15:59:49 -0800

    Add PEDAC for Exercise 4

---
+++ small_problems/easy_3/04.rb
@@ -0,0 +1,18 @@
+=begin
+--- P
+Inputs:
+- Two numbers
+Output:
+- Product of the input multiplied together
+
+Implicit rules:
+- Two numbers are given
+--- E
+multiply(5, 3) == 15
+multiply(0, 4) == 0
+multiply(7, 0) == 0
+--- D
+
+--- A
+- Return the product of the two integers passed to the method
+=end
