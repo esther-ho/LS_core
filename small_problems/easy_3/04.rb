4010d09b89bbe827e3d3a96162f101b02609a875
Esther Ho <ekyho@ucdavis.edu>
2022-12-25 16:21:11 -0800

    Add example for an array

--- small_problems/easy_3/04.rb
+++ small_problems/easy_3/04.rb
@@ -28,3 +28,19 @@ end
 puts multiply(5, 3) == 15
 puts multiply(0, 4) == 0
 puts multiply(7, 0) == 0
+
+# `Array#*` passes the reference value of all elements in the array to the method
+# `#*` copies the elements and reference values and adds them to the array
+arr = ['h','i']
+puts "Original array: #{arr}"
+puts "Original array object id: #{arr.object_id}"
+puts "Original array, first element id: #{arr[0].object_id}"
+
+modified_arr = multiply(arr, 2)
+puts "Modified array: #{modified_arr}"
+puts "Modified array object id: #{modified_arr.object_id}"
+puts "Modified array, first element id: #{modified_arr[0].object_id}"
+puts "Modified array, third element id: #{modified_arr[2].object_id}"
+
+modified_arr[0].upcase!
+puts "Modified array after #upcase: #{modified_arr}"
