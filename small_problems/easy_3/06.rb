c9704fed777fd3a44993f8342a2dc43aa4a34b51
Esther Ho <ekyho@ucdavis.edu>
2022-12-26 15:40:43 -0800

    Add Exercise 6

--- small_problems/easy_3/06.rb
+++ small_problems/easy_3/06.rb
@@ -20,3 +20,12 @@ xor?(5.even?, 4.odd?) == false
   - Return true
 - Else, return false
 =end
+
+def xor?(val1, val2)
+  !!val1 != !!val2
+end
+
+puts xor?(5.even?, 4.even?) == true
+puts xor?(5.odd?, 4.odd?) == true
+puts xor?(5.odd?, 4.even?) == false
+puts xor?(5.even?, 4.odd?) == false
