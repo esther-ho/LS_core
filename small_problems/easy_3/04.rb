17ea4e3819c8a40528164e8805ae1d72c25e1a61
Esther Ho <ekyho@ucdavis.edu>
2022-12-25 16:00:18 -0800

    Add Exercise 4

--- small_problems/easy_3/04.rb
+++ small_problems/easy_3/04.rb
@@ -16,3 +16,11 @@ multiply(7, 0) == 0
 --- A
 - Return the product of the two integers passed to the method
 =end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+p multiply(5, 3) == 15
+p multiply(0, 4) == 0
+p multiply(7, 0) == 0
