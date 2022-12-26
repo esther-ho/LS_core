a3c060fc35d7acb1f8a544af004a03030403316c
Esther Ho <ekyho@ucdavis.edu>
2022-12-26 14:14:44 -0800

    Add PEDAC for Exercise 5

---
+++ small_problems/easy_3/05.rb
@@ -0,0 +1,18 @@
+=begin
+--- P
+Inputs:
+- One number
+Outputs:
+- The square of the given number
+
+Implicit rules:
+- Only numeric values are given
+- Method used should be similar to `multiply`
+--- E
+square(5) == 25
+square(-8) == 64
+--- D
+
+--- A
+- Multiply the number by itself
+=end
