07dde23dcef9046e27dbcc21424ed080dda7f8c0
Esther Ho <ekyho@ucdavis.edu>
2022-12-26 15:44:09 -0800

    Refactor code

--- small_problems/easy_3/06.rb
+++ small_problems/easy_3/06.rb
@@ -22,10 +22,11 @@ xor?(5.even?, 4.odd?) == false
 =end
 
 def xor?(val1, val2)
-  !!val1 != !!val2
+  !val1 != !val2
 end
 
 puts xor?(5.even?, 4.even?) == true
 puts xor?(5.odd?, 4.odd?) == true
 puts xor?(5.odd?, 4.even?) == false
 puts xor?(5.even?, 4.odd?) == false
+puts xor?('a', false) == true
