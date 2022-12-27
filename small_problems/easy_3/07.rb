c5a4e50d31dd20ea87d6269d76abd56aadce4423
Esther Ho <ekyho@ucdavis.edu>
2022-12-27 15:50:58 -0800

    Add PEDAC for Exercise 7

---
+++ small_problems/easy_3/07.rb
@@ -0,0 +1,26 @@
+=begin
+--- P
+Inputs:
+- An array
+Outputs:
+- An array with only odd-place elements, e.g. 1st (index 0), 3rd (index 2) etc
+Implcit rules:
+- Elements in the array can be any literal object
+- Array can be empty, in which case the output is an empty array
+- No nested arrays
+--- E
+oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+oddities(['abc', 'def']) == ['abc']
+oddities([123]) == [123]
+oddities([]) == []
+oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
+--- D
+- Inputs and outputs are arrays
+--- A
+- If array is empty,
+  - Return an empty array
+- Else,
+  - Select the elements in the array with even indexes
+  - Return the resultant array
+=end
