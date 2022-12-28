d5838f7a505157dc00c1bb177c9884a6720253cb
Esther Ho <ekyho@ucdavis.edu>
2022-12-27 17:14:58 -0800

    Add solution for Exercise 8 - Further explorations

--- small_problems/easy_3/08.rb
+++ small_problems/easy_3/08.rb
@@ -33,3 +33,24 @@ p palindrome?('madam') == true
 p palindrome?('Madam') == false          # (case matters)
 p palindrome?("madam i'm adam") == false # (all characters matter)
 p palindrome?('356653') == true
+
+# `arr.reverse == arr` for array palindromes
+# `item.reverse == item` for array or string palindromes
+
+# Coding palindrome method from scratch
+def all_palindrome?(item)
+  item = item.chars unless item.is_a?(Array)
+  left, right = 0, -1
+  while left < item.size / 2
+    return false if item[left] != item[right]
+    left += 1
+    right -= 1
+  end
+  true
+end
+
+p all_palindrome?([1, 2, 1])
+p all_palindrome?('madam') == true
+p all_palindrome?('Madam') == false          # (case matters)
+p all_palindrome?("madam i'm adam") == false # (all characters matter)
+p all_palindrome?('356653') == true
