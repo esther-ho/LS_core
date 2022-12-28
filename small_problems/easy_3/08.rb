22d277140046da52c6d82a07dcfbb6652b662fc1
Esther Ho <ekyho@ucdavis.edu>
2022-12-27 16:33:58 -0800

    Add Exercise 8

--- small_problems/easy_3/08.rb
+++ small_problems/easy_3/08.rb
@@ -24,3 +24,12 @@ palindrome?('356653') == true
   - Return true
 - Else, return false.
 =end
+
+def palindrome?(string)
+  string.reverse == string
+end
+
+p palindrome?('madam') == true
+p palindrome?('Madam') == false          # (case matters)
+p palindrome?("madam i'm adam") == false # (all characters matter)
+p palindrome?('356653') == true
