7ad4e67e9cb1917c96d8a710e48ad9a1e341c990
Esther Ho <ekyho@ucdavis.edu>
2022-12-27 16:33:40 -0800

    Add PEDAC for Exercise 8

---
+++ small_problems/easy_3/08.rb
@@ -0,0 +1,26 @@
+=begin
+--- P
+Inputs:
+- A string containing multiple characters.
+Outputs:
+- Return true if string is a palindrome
+  - String is the same when read from the front or back.
+- Return false otherwise.
+Implicit rules:
+- If string contains alphabetical characters,
+  - The case must be the same when read from front or back
+- If the string contains punctuation or spaces,
+  - They should be in the same place in the string when read from front/back
+--- E
+palindrome?('madam') == true
+palindrome?('Madam') == false          # (case matters)
+palindrome?("madam i'm adam") == false # (all characters matter)
+palindrome?('356653') == true
+--- D
+
+--- A
+- Reverse the original string
+- If the original string matches the reversed string,
+  - Return true
+- Else, return false.
+=end
