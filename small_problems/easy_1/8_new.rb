7dc484ef8910164b79de093b0cac19a4065715b7
Esther Ho <ekyho@ucdavis.edu>
2023-01-02 16:23:03 -0800

    Add Exercise 8

--- small_problems/easy_1/8_new.rb
+++ small_problems/easy_1/8_new.rb
@@ -8,7 +8,6 @@ Output:
 Implicit rules:
 - Argument will be a valid positive whole number
 - Any leading zeros (0s in front) of reversed number are removed
-- Leading zeros in input will not be included
 --- E
 reversed_number(12345) == 54321
 reversed_number(12213) == 31221
@@ -25,3 +24,14 @@ reversed_number(1) == 1
 - Join the digits in the array into a string of numbers
 - Convert the string to an integer
 =end
+
+def reversed_number(number)
+  number.to_s.split('').reverse.join.to_i
+end
+
+p reversed_number(12345) == 54321
+p reversed_number(12213) == 31221
+p reversed_number(456) == 654
+p reversed_number(12000) == 21 # No leading zeros in return value!
+p reversed_number(12003) == 30021
+p reversed_number(1) == 1
