4d4f79b9a9b69b6855c059d0c9e03f97e640588e
Esther Ho <ekyho@ucdavis.edu>
2023-01-04 19:20:47 -0800

    Add comments for Exercise 9

--- small_problems/easy_3/09.rb
+++ small_problems/easy_3/09.rb
@@ -37,3 +37,14 @@ p palindromic_number?(34543) == true
 p palindromic_number?(123210) == false
 p palindromic_number?(22) == true
 p palindromic_number?(5) == true
+
+=begin
+- If a number has leading zeros,
+  - Ruby treats the number as an octal (base 8) number.
+- Converting the number to a string converts it to its decimal (base 10) number
+  - E.g. `0020.to_s` => "16"
+- If converted to a string with place-value representation of base 8,
+  - You retain the number "20" but lose the leading zeros
+- You can determine if a *string* of numbers with leading 0s is a palindrome
+  - But not a number with leading 0s since leading 0s are removed
+=end
