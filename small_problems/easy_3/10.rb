9edc5ad7f3bc04617d8af27f28798b9c8de2d9dd
Esther Ho <ekyho@ucdavis.edu>
2023-01-05 14:20:22 -0800

    Add comments for Exercise 10 - Further exploration

--- small_problems/easy_3/10.rb
+++ small_problems/easy_3/10.rb
@@ -40,3 +40,25 @@ p uppercase?('Four Score') == false
 p uppercase?('FOUR SCORE') == true
 p uppercase?('4SCORE!') == true
 p uppercase?('') == true
+
+=begin
+Further exploration:
+If `uppercase?` were to return `false` for empty strings,
+- The user is unable to tell if the method returns `false` because:
+  - Not all alphabetic characters are uppercase, or
+  - No alphabetic characters are present
+Similarly, if `uppercase?` returned `true` for empty strings,
+- The user is unable to tell if:
+  - All alphabetic characters are uppercase, or
+  - No alphabetic characters are present
+Ways to distinguish if the return value is due to empty string or otherwise
+1. Check if string contains alphabetic characters before `uppercase?`
+  - Eliminate strings without alphabetic characters
+  - Check for uppercase after elimination
+2. Both `uppercase?` and `lowercase?` should return `true` for empty strings
+  - Empty strings include strings with only non-alphabetic characters
+  - If a string returns `true` for both methods,
+    - We can conclude it does not contain alphabetic characters
+3. Separate non-boolean (`true`/`false`) value if string is either:
+  - Empty or only contains non-alphabetic characters
+=end
