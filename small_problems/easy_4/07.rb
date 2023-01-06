183b64fd5369144a72bbfbaf378ba0427f22993d
Esther Ho <ekyho@ucdavis.edu>
2023-01-06 11:47:11 -0800

    Add PEDAC for Exercise 7

---
+++ small_problems/easy_4/07.rb
@@ -0,0 +1,26 @@
+=begin
+--- P
+Input:
+- A string containing only numeric characters
+Output:
+- An integer form of the numeric string
+Implicit rules:
+- The input can be modified
+--- E
+string_to_integer('4321') == 4321
+string_to_integer('570') == 570
+--- D
+- Hash containing:
+  - Keys as individual numeric strings e.g. ('1')
+  - Values as individual numeric digits e.g. (1)
+- Array of numeric characters
+- Array of integer digits
+--- A
+- Initialize a hash of key-value pairs of numeric strings and numbers
+- Split the string into its individual characters
+- Iterate through each value in the resultant array
+- For each iteration,
+  - Multiply the current total by 10
+  - Return the appropriate numeric value from the hash based on the key
+  - Add the numeric value
+=end
