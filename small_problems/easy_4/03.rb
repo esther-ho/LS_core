c51497d7fd7ae9d8cf185f7ae8ae3345bcd86712
Esther Ho <ekyho@ucdavis.edu>
2023-01-05 16:04:54 -0800

    Add PEDAC for Exercise 3

---
+++ small_problems/easy_4/03.rb
@@ -0,0 +1,36 @@
+=begin
+--- P
+Input:
+- A year in whole numbers
+Output:
+- Return `true` if year is a leap year
+- Return `false` if year is not a leap year
+Explicit rules:
+- Leap years are divisible by either 4 or 400
+- Leap years are not divisible by 100
+Implicit rules:
+- Only years greater than 0 are given
+- Only numeric strings are given
+--- E
+leap_year?(2016) == true
+leap_year?(2015) == false
+leap_year?(2100) == false
+leap_year?(2400) == true
+leap_year?(240000) == true
+leap_year?(240001) == false
+leap_year?(2000) == true
+leap_year?(1900) == false
+leap_year?(1752) == true
+leap_year?(1700) == false
+leap_year?(1) == false
+leap_year?(100) == false
+leap_year?(400) == true
+--- D
+
+--- A
+- If divisible by 400,
+  - Return `true`
+- If divisible by 4 BUT NOT 100,
+  - Return `true`
+- Else, return `false`
+=end
