537fc0e855ceff5d239f159b39d610defdb40980
Esther Ho <ekyho@ucdavis.edu>
2023-01-05 16:14:46 -0800

    Refactor solution for Exercise 3

--- small_problems/easy_4/03.rb
+++ small_problems/easy_4/03.rb
@@ -36,8 +36,7 @@ leap_year?(400) == true
 =end
 
 def leap_year?(year)
-  return true if year % 400 == 0
-  (year % 4 == 0) != (year % 100 == 0)
+  year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
 end
 
 p leap_year?(2016) == true
