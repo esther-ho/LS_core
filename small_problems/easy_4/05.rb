4793c5af69182dde2ff770d1e0e7898426d0d5f1
Esther Ho <ekyho@ucdavis.edu>
2023-01-06 10:11:59 -0800

    Add Exercise 5

--- small_problems/easy_4/05.rb
+++ small_problems/easy_4/05.rb
@@ -19,13 +19,25 @@ multisum(1000) == 234168
 --- D
 - Store relevant multiples in an array
 --- A
-- Initialize a range from 1 to input number
 - Create an empty array called `multiples`
+- Initialize a range from 1 to input number
 - Iterate through the range
 - For each iteration,
-  - If the number is divisible by 3,
-    - Push the number into the `multiples` array
-  - Else, if the number is divisible by 5
+  - If the number is divisible by 3, or divisible by 5
     - Push the number into the `multiples` array
 - Sum the multiples in the array
 =end
+
+def multisum(number)
+  multiples = []
+  (1..number).each do |num|
+    multiples << num if num % 3 == 0 || num % 5 == 0
+  end
+
+  multiples.sum
+end
+
+p multisum(3) == 3
+p multisum(5) == 8
+p multisum(10) == 33
+p multisum(1000) == 234168
