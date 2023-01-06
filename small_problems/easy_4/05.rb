100763a52b0150e2c6d018161fb6ec9961f2491b
Esther Ho <ekyho@ucdavis.edu>
2023-01-06 10:17:34 -0800

    Add comments for Exercise 5

--- small_problems/easy_4/05.rb
+++ small_problems/easy_4/05.rb
@@ -41,3 +41,10 @@ p multisum(3) == 3
 p multisum(5) == 8
 p multisum(10) == 33
 p multisum(1000) == 234168
+
+=begin
+Alternative using `Enumerable#inject`
+def multisum(number)
+  (1..number).select{ |num| num % 3 == 0 || num % 5 || 0 }.inject(:+)
+end
+=end
