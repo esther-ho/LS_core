c9054ebf48b477d2ea885ee7728bf4294ce31828
Esther Ho <ekyho@ucdavis.edu>
2023-01-05 16:12:46 -0800

    Add Exercise 3

--- small_problems/easy_4/03.rb
+++ small_problems/easy_4/03.rb
@@ -34,3 +34,22 @@ leap_year?(400) == true
   - Return `true`
 - Else, return `false`
 =end
+
+def leap_year?(year)
+  return true if year % 400 == 0
+  (year % 4 == 0) != (year % 100 == 0)
+end
+
+p leap_year?(2016) == true
+p leap_year?(2015) == false
+p leap_year?(2100) == false
+p leap_year?(2400) == true
+p leap_year?(240000) == true
+p leap_year?(240001) == false
+p leap_year?(2000) == true
+p leap_year?(1900) == false
+p leap_year?(1752) == true
+p leap_year?(1700) == false
+p leap_year?(1) == false
+p leap_year?(100) == false
+p leap_year?(400) == true
