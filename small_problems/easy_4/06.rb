79f6df688e9330cc772000632ff4e2e4f9522fcc
Esther Ho <ekyho@ucdavis.edu>
2023-01-06 11:19:05 -0800

    Add comments for Exercise 6

--- small_problems/easy_4/06.rb
+++ small_problems/easy_4/06.rb
@@ -25,12 +25,29 @@ running_total([]) == []
   - Return the total for each element
 =end
 
-def running_total(number_array)
-  total = 0
-  number_array.map { |num| total += num }
-end
+# def running_total(number_array)
+#   total = 0
+#   number_array.map { |num| total += num }
+# end
 
 p running_total([2, 5, 13]) == [2, 7, 20]
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
 p running_total([3]) == [3]
 p running_total([]) == []
+
+=begin
+Alternative using `Enumerable#each_with_object`:
+def running_total(array)
+  array.each_with_object([]) { |val, arr| arr << val + arr.last.to_i }
+end
+
+Alternative using `Enumerable#inject`:
+def running_total(array)
+  array.inject([]) { |arr, val| arr << val + arr.last.to_i }
+  end
+end
+
+Alternative using `Enumerable#with_index`
+def running_total(array)
+  array.map.with_index { |_, i| array[0..i].sum }
+=end
