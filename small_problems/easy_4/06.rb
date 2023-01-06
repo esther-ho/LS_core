84b5ffb576c7bc25b845292e41c1f35b5fc2571c
Esther Ho <ekyho@ucdavis.edu>
2023-01-06 10:31:29 -0800

    Add PEDAC for Exercise 6

---
+++ small_problems/easy_4/06.rb
@@ -0,0 +1,26 @@
+=begin
+--- P
+Input:
+- Array of whole numbers
+Output:
+- Same number of elements as original array
+- Each element is the running total of the original array
+Implicit rules:
+- Elements do not have to be sorted.
+- Numbers are positive
+- Empty arrays result in an empty array as output
+--- E
+running_total([2, 5, 13]) == [2, 7, 20]
+running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+running_total([3]) == [3]
+running_total([]) == []
+--- D
+- Original array of numbers as input
+- Array of running totals as output
+--- A
+- Set total = 0
+- Iterate through the input array
+- For each iteration,
+  - Add the number in the current iteration to `total`
+  - Return the total for each element
+=end
