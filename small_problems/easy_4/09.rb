6beb9989c6e27891de5ee480b11c682b1312c984
Esther Ho <ekyho@ucdavis.edu>
2023-01-08 18:21:30 -0800

    Add solution to Exercise 9

--- small_problems/easy_4/09.rb
+++ small_problems/easy_4/09.rb
@@ -18,8 +18,29 @@ integer_to_string(5000) == '5000'
 - Initialize a hash containing integers and strings as keys and values
 - Split given integer into an array containing its digits
 - Iterate through the array
-- Set an empty string `num_string` as the initial value
+- Set an empty string `string` as the initial value
 - For each iteration,
   - Return the string value of the corresponding integer from the hash
-  - Append the string value to `num_string`
+  - Prepend the string value to `string`
 =end
+
+DIGITS = ((0..9).zip('0'..'9')).to_h
+
+def integer_to_string(integer)
+  integer.digits.inject('') do |string, value|
+    string.prepend(DIGITS[value])
+  end
+end
+
+=begin
+Alternative:
+def integer_to_string(integer)
+  integer.digits.each_with_object('') do |value, string|
+    string.prepend(DIGITS[value])
+  end
+end
+=end
+
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'
