88b7c22fea10dd5199b37edf071045949b901fd0
Esther Ho <ekyho@ucdavis.edu>
2023-01-08 17:24:58 -0800

    Add Exercise 8

--- small_problems/easy_4/08.rb
+++ small_problems/easy_4/08.rb
@@ -18,12 +18,29 @@ string_to_signed_integer('+100') == 100
 --- D
 - Hash of numeric string and their corresponding integers
 --- A
-- Initialize a variable `sign` with an empty string
 - Check the first character of the string
 - If the first character is not a digit,
   - Remove the first character and assign it to `sign`
 - Call `string_to_integer` on the remaining characters
 - If `sign` is equal to `-`
-  - Multiply the resulting integer by one
+  - Negate the integer
 - Else, return the resulting integer
 =end
+
+DIGITS = (('0'..'9').zip(0..9)).to_h
+
+def string_to_integer(num_str)
+  num_str.chars.inject(0) do |result, str|
+    result * 10 + DIGITS[str]
+  end
+end
+
+def string_to_signed_integer(num_str)
+  sign = num_str.slice!(0) if %w(+ -).include?(num_str[0])
+  integer = string_to_integer(num_str)
+  sign == '-' ? -integer : integer
+end
+
+p string_to_signed_integer('4321') == 4321
+p string_to_signed_integer('-570') == -570
+p string_to_signed_integer('+100') == 100
