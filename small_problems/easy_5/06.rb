b66b5d8f47190d837c22896c75f13125b4a610bf
Esther Ho <ekyho@ucdavis.edu>
2023-01-11 16:09:29 -0800

    Add comments for alternate solution to Exercise 6

--- small_problems/easy_5/06.rb
+++ small_problems/easy_5/06.rb
@@ -20,10 +20,35 @@ word_sizes('') == {}
 - Hash to store the sizes of words in teh string and counts of each size
 - Split the string into individual words in an array, using space as a delimiter
 --- A
-- Return an empty hash if the string is empty
 - Split the string into individual words in an array, using space as a delimiter
 - Iterate through the array
 - For each iteration,
   - Return the length of the current string
 - Count the number of occurences of each string size
+=end
+
+def word_sizes(string)
+  string.split.map(&:size).tally
+end
+
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') ==
+  { 3 => 5, 6 => 1, 7 => 2 }
+p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('') == {}
+# An empty array will return an empty hash when calling `#tally`
+
+=begin
+Alternative using Hash:
+def word_size(string)
+  count = Hash.new(0)
+  string.split.each { |word| count[word.size] += 1 }
+  count
+end
+
+- Sending an argument to `Hash::new` sets it as the default value
+  - It forces references to non-existing keys to return the default value
+  - e.g. `Hash.new(0)` sets all keys to have the default value of 0 (return 0)
+  - We need to set the default to 0 since `1` cannot be added to `nil`
+- `Hash#default=` can also be used to set the default value of keys
 =end
