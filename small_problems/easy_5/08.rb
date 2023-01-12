84f78c367ae43c554b67f1b731b66e4b1537f1bf
Esther Ho <ekyho@ucdavis.edu>
2023-01-12 15:47:55 -0800

    Add solution to Exercise 8

--- small_problems/easy_5/08.rb
+++ small_problems/easy_5/08.rb
@@ -21,10 +21,26 @@ alphabetic_number_sort((0..19).to_a) == [
 - Initialize a hash where:
   - Keys are the integers from 0 to 19
   - Values are the corresponding english word for the number
-- Iterate through the number array
-- For each iteration,
-  - Compare the english word of the current number to the next number
-  - If the word value of the current number is larger,
-    - Swap the current number with the next number
-- Return the numbers array
+- Sort the number array by comparing:
+  - Word value of current number to that of the next number
 =end
+
+NUM_WORD = {
+  0 => 'zero', 1 => 'one', 2 => 'two',
+  3 => 'three', 4 => 'four', 5 => 'five',
+  6 => 'six', 7 => 'seven', 8 => 'eight',
+  9 => 'nine', 10 => 'ten', 11 => 'eleven',
+  12 => 'twelve', 13 => 'thirteen',
+  14 => 'fourteen', 15 => 'fifteen',
+  16 => 'sixteen', 17 => 'seventeen',
+  18 => 'eighteen', 19 => 'nineteen'
+}
+
+def alphabetic_number_sort(nums)
+  nums.sort { |n1, n2| NUM_WORD[n1] <=> NUM_WORD[n2] }
+end
+
+p alphabetic_number_sort((0..19).to_a) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+]
