cbc87b63779afbe7423d40d40aaa5e772a8c06fb
Esther Ho <ekyho@ucdavis.edu>
2023-01-16 16:14:30 -0800

    Add PEDAC for Exercise 11

---
+++ small_problems/easy_5/11.rb
@@ -0,0 +1,22 @@
+=begin
+--- P
+Input:
+- A positive whole number
+Output:
+- An array of digits in the number
+Implicit rules:
+- Digits in the array appear in the order of how they appear in the number
+  - E.g. 444 => [4, 4, 4]
+- Repeated digits are not omitted
+--- E
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
+--- D
+- An array containing the digits in the number
+--- A
+- Convert the number to its string form
+- Split the string into its individual digits
+- Convert each digit into its integer form
+=end
