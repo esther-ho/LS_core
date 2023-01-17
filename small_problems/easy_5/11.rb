21f2afd7cba87b769332d4bf52fdef3df53f283a
Esther Ho <ekyho@ucdavis.edu>
2023-01-16 16:59:33 -0800

    Add alternate solutions to Exercise 11

--- small_problems/easy_5/11.rb
+++ small_problems/easy_5/11.rb
@@ -29,3 +29,23 @@ puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
 puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
+
+=begin
+Alternatives:
+- Divide integer by 10 and append remainder to an array
+
+def digit_list(num)
+  digits = []
+  while num > 0
+    num, remainder = num.divmod(10)
+    digits.unshift(remainder)
+  end
+  digits
+end
+
+- Use Ruby's `Integer#digits` method
+
+def digit_list(num)
+  num.digits.reverse
+end
+=end
