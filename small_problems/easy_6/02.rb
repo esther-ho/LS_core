473a60500385433254cd53e4fdbe514f3c4d4fe7
Esther Ho <ekyho@ucdavis.edu>
2023-01-17 14:29:51 -0800

    Add PEDAC for Exercise 2

---
+++ small_problems/easy_6/02.rb
@@ -0,0 +1,25 @@
+=begin
+--- P
+Input:
+- An array of strings with alphabetic characters
+Output:
+- The given string with vowels (a, e, i, o u) removed
+Implicit rules:
+- If the string is empty after vowels are removed,
+  - Return the empty string
+- The method of removing vowels is case-insensitive
+- The original lettercase of the string is preserved
+- No mention if the original array can be mutated
+--- E
+remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+--- D
+- The given array with one or more strings
+- The output array with equal number of strings as the given array
+--- A
+- Iterate through the array
+- For each iteration,
+  - Delete vowels (case-insensitive)
+- Return the new modified array 
+=end
