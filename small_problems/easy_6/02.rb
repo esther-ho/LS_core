2ae38ddfe7b6754e2103ac891a19d2789a5b3c33
Esther Ho <ekyho@ucdavis.edu>
2023-01-18 15:50:28 -0800

    Add alternate solution to Exercise 2

--- small_problems/easy_6/02.rb
+++ small_problems/easy_6/02.rb
@@ -31,3 +31,12 @@ end
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+=begin
+Alternatives:
+- Substitute vowels (case-insensitive) with an empty string
+
+def remove_vowels(strings)
+  strings.map { |string| string.gsub(/[aeiou]/i, '')}
+end
+=end
