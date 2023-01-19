f847c4bceaa421ddab826c3aae57c5cf29824701
Esther Ho <ekyho@ucdavis.edu>
2023-01-19 15:55:08 -0800

    Add solution to Exercise 4

--- small_problems/easy_6/04.rb
+++ small_problems/easy_6/04.rb
@@ -34,15 +34,45 @@ list == [] # true
 - The original input is given as an array of elements
 - The output is the mutated version of the original array
 --- A
-- Return an empty array if the input array is empty
 - Find the mid-point of the array and save it as `mid`
 - Iterate through the original array
-- Assign front as the first element (index 0)
-- Assign back as the last element (index -1)
+- Assign front as '0'
+- Assign back as '-1'
 - While the value of `front` is less than `mid`
 - For each iteration,
-  - Swap the front and back elements
+  - Swap the elements at indexes `front` and `back`
   - Increment `front` by 1
   - Decrease `back` by 1
 - Return the modified array
 =end
+
+def reverse!(array)
+  mid = array.size / 2
+  front = 0
+  back = -1
+
+  while front < mid
+    array[front], array[back] = array[back], array[front]
+    front += 1
+    back -= 1
+  end
+  array
+end
+
+list = [1, 2, 3, 4]
+result = reverse!(list)
+p result == [4, 3, 2, 1] # true
+p list == [4, 3, 2, 1] # true
+p list.object_id == result.object_id # true
+
+list = %w(a b e d c)
+p reverse!(list) == ["c", "d", "e", "b", "a"] # true
+p list == ["c", "d", "e", "b", "a"] # true
+
+list = ['abc']
+p reverse!(list) == ["abc"] # true
+p list == ["abc"] # true
+
+list = []
+p reverse!(list) == [] # true
+p list == [] # true
