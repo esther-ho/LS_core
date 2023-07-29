c34235b90057d6a14d816be3e0e0a67278fba39d
Esther Ho <ekyho@ucdavis.edu>
2023-07-28 21:28:28 -0700

    Update Easy 7 - Problem 3

--- exercises/small_problems/easy_7/03.rb
+++ exercises/small_problems/easy_7/03.rb
@@ -8,6 +8,7 @@ Explicit rules:
 - String contains words of any sequence with non-blank characters
 Implicit rules:
 - If words do not begin with a letter, the word is not capitalized
+- If there are capitalized letters that are not the first letter, convert them to their lowercase form
 --- E
 word_cap('four score and seven') == 'Four Score And Seven'
 word_cap('the javaScript language') == 'The Javascript Language'
@@ -25,8 +26,11 @@ word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
 
 def word_cap(string)
   string.split.map(&:capitalize).join(' ')
+  # Alternatives
+  # string.split.map { |word| word[0].upcase + word[1..-1].downcase } * ' '
+  # string.downcase.gsub(/^[a-z]|(?<= )([a-z])/) { |char| char.upcase }
 end
 
-word_cap('four score and seven') #== 'Four Score And Seven'
-word_cap('the javaScript language') #== 'The Javascript Language'
-word_cap('this is a "quoted" word') #== 'This Is A "quoted" Word'
+word_cap('four score and seven') == 'Four Score And Seven'
+word_cap('the javaScript language') == 'The Javascript Language'
+word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
