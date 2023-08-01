5b48c1e01050d5da85c72a4e173109117cb99988
Esther Ho <ekyho@ucdavis.edu>
2023-07-31 20:59:20 -0700

    Add solution to problem 14

---
+++ RB110/problem_14.rb
@@ -0,0 +1,14 @@
+# Return an array containing the colors of the fruits and sizes of the vegetables
+# The sizes should be uppercase and colors should be capitalized
+
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+
+hsh.map do |_, v|
+  v[:type] == 'fruit' ? v[:colors].map(&:capitalize) : v[:size].upcase
+end
