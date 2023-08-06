fc7c5119a3cdd35df56791c1f9bbed3657680d6f
Esther Ho <ekyho@ucdavis.edu>
2023-08-05 17:54:08 -0700

    Add features to deal cards and update hand total

--- rb110/lesson_3/21_project/twentyone.rb
+++ rb110/lesson_3/21_project/twentyone.rb
@@ -4,7 +4,10 @@ require 'pry'
 CONFIG = Psych.load_file('config_twentyone.yml')
 SUITS = CONFIG['suits']
 CARDS = CONFIG['cards']
+ACE_TOTALS = CONFIG['ace_totals']
 PLAYERS = [:dealer, :player]
+DEAL_TIMES = 2
+MAX_TOTAL = 21
 
 def initialize_deck
   CARDS.keys.product(SUITS).map { |card| card.join(' ') }.shuffle
@@ -45,9 +48,54 @@ def joinor(hand, delimiter: ', ', word: 'and')
   end
 end
 
+def add_to_hand!(card, hand)
+  hand.push(card)
+end
+
+def deal!(deck, hands, hand_totals)
+  DEAL_TIMES.times do
+    PLAYERS.each do |player|
+      card = deck.pop
+      add_to_hand!(card, hands[player])
+    end
+  end
+
+  PLAYERS.each do |player|
+    hand_totals[player] = update_total(hands[player])
+  end
+end
+
+def update_total(hand)
+  totals = possible_totals(hand)
+  valid_totals = totals.select { |total| total < MAX_TOTAL }
+  valid_totals.empty? ? totals.min : valid_totals.max
+end
+
+def possible_totals(hand)
+  card_values = find_card_values(hand)
+  return [card_values.sum] unless ace?(hand)
+
+  aces, other_cards = separate_aces(hand)
+  aces = ACE_SUMS.fetch(aces.size)
+  aces.map { |ace| ace + other_cards.sum }
+end
+
+def ace?(hand)
+  !!hand.include?(CARDS['Ace'])
+end
+
+def separate_aces(hand)
+  hand.partition { |card| card == CARDS['Ace'] }
+end
+
+def find_card_values(hand)
+  hand.map { |card| CARDS[card[0..-3]] }
+end
+
 deck = initialize_deck
 hands = initialize_hands
 hand_totals = initialize_hand_totals
 dealer, player = PLAYERS
 
+deal!(deck, hands, hand_totals)
 display_hands(hands, hand_totals, hide_one: true)
