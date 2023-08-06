f87f13f8e44abe74531bbeae430ca34b3dcb1695
Esther Ho <ekyho@ucdavis.edu>
2023-08-05 17:30:53 -0700

    Add features to display hands and hide dealer's second card

--- rb110/lesson_3/21_project/twentyone.rb
+++ rb110/lesson_3/21_project/twentyone.rb
@@ -18,9 +18,36 @@ def initialize_hand_totals
   PLAYERS.each_with_object({}) { |player, hsh| hsh[player] = 0 }
 end
 
+def display_hands(hands, hand_totals, hide_one: false)
+  display = (hide_one ? CONFIG['hidden_hands'] : CONFIG['visible_hands'])
+
+  puts format(display, *visible_hands(hands, hand_totals, hide_one))
+end
+
+def visible_hands(hands, hand_totals, hide_one)
+  dealer_hand, player_hand = hands.values
+  dealer_total, player_total = hand_totals.values
+
+  if hide_one
+    dealer_hand = hands[PLAYERS[0]].first
+    dealer_total = nil
+  end
+
+  [joinor(dealer_hand), dealer_total, joinor(player_hand), player_total]
+end
+
+def joinor(hand, delimiter: ', ', word: 'and')
+  return hand if !hand.is_a?(Array)
+
+  case hand.size
+  when 2 then hand.join(" #{word} ")
+  else "#{hand[0..-2].join(delimiter)} #{word} #{hand[-1]}"
+  end
+end
+
 deck = initialize_deck
 hands = initialize_hands
 hand_totals = initialize_hand_totals
 dealer, player = PLAYERS
 
-
+display_hands(hands, hand_totals, hide_one: true)
