5acec6c5b444b1ed9eb61ad444d1e049ca013bdb
Esther Ho <ekyho@ucdavis.edu>
2023-08-21 22:03:24 -0700

    Add solution to Further explorations problem for Exercise 10

--- exercises/small_problems/easy_8/10.rb
+++ exercises/small_problems/easy_8/10.rb
@@ -34,6 +34,12 @@ def average(numbers)
   sum / numbers.size
 end
 
+# Further exploration
+
+# def average(numbers)
+#   numbers.inject(&:+) / numbers.size.to_f
+# end
+
 puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
