2a5c4d118266cb37fa6e69b3b3a46750f87f49fb
Esther Ho <ekyho@ucdavis.edu>
2023-08-27 22:05:58 -0700

    Add alternative solution to Exercise 10

--- exercises/small_problems/medium_1/10.rb
+++ exercises/small_problems/medium_1/10.rb
@@ -36,13 +36,25 @@ def fibonacci_last(n)
     first, last = [last, first + last]
   end
 
-  p last.to_s[-1].to_i
+  last.to_s[-1].to_i
 end
 
-fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
-fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
-fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
-fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
-fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+# Alternative
+
+# def fibonacci_last(n)
+#   last_2 = [1, 1]
+  
+#   3.upto(n) do
+#     last_2 = [last_2[1], (last_2[0] + last_2[1]) % 10]
+#   end
+
+#   last_2[1]
+# end
+
+p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
 # fibonacci_last(123456789) # -> 4
 
