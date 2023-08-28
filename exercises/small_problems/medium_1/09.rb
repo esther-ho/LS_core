0c11f312479f9f3f9a515d74594a10dcde7faed8
Esther Ho <ekyho@ucdavis.edu>
2023-08-27 21:47:07 -0700

    Add alternative solution to Exercise 9

--- exercises/small_problems/medium_1/09.rb
+++ exercises/small_problems/medium_1/09.rb
@@ -35,6 +35,18 @@ def fibonacci(n)
   fibonacci_nums[-1]
 end
 
+# Alternative
+
+# def fibonacci(n)
+#   first, second = [1, 1]
+
+#   3.upto(n) do
+#     first, second = [second, first + second]
+#   end
+
+#   second
+# end
+
 p fibonacci(20) == 6765
 p fibonacci(100) == 354224848179261915075
 p fibonacci(100_001) # => 4202692702.....8285979669707537501
