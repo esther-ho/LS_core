159cd648d9f40bedf9a1142b4eb4a14d7bad8d83
Esther Ho <ekyho@ucdavis.edu>
2023-09-04 16:54:58 -0700

    Add solution to Exercise 1

---
+++ exercises/small_problems/01.rb
@@ -0,0 +1,56 @@
+=begin
+Input: a text file with %{...} strings
+Output: output of the modified text file, where:
+- random words of the appropriate text are added into the text
+
+Rules:
+- Replacement words can be reused
+
+--- E
+The %{adjective} brown %{noun} %{adverb}
+%{verb} the %{adjective} yellow
+%{noun}, who %{adverb} %{verb} his
+%{noun} and looks around.
+
+The sleepy brown cat noisily
+licks the sleepy yellow
+dog, who lazily licks his
+tail and looks around.
+
+'sleepy' and 'licks' are used twice
+
+--- D
+Input: a text file with %{...} strings
+In-between:
+- a hash where,
+  - keys are symbols representing the types of words
+  - values are arrays containing words of that type
+Output: output of a modified text file
+
+--- A
+- Initialize a hash, where:
+  - keys are symbols representing the types of words
+  - values are arrays containing a list of words of that type
+- Read the text file
+- Replace any %{...} strings
+  - For each string, replace it with a word randomly selected from the specific list
+- Print the modified text file
+=end
+
+def madlibs(text)
+  words_hash = {
+    adjective: %w(quick lazy sleepy ugly),
+    noun: %w(fox dog head leg),
+    verb: %w(jumps lifts bites licks),
+    adverb:%w(easily lazily noisily excitedly)
+  }
+
+  text = text.gsub(/%{\w+}/) do |word| 
+    word % words_hash.transform_values(&:sample)
+  end
+
+  puts text
+end
+
+text = File.read('madlibs.txt')
+madlibs(text)
