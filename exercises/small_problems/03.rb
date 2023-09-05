7e9374efbd50040e701b44097d1cdad3e70acf9a
Esther Ho <ekyho@ucdavis.edu>
2023-09-04 18:34:18 -0700

    Add solution to Further exploration problem to Exercise 3

--- exercises/small_problems/03.rb
+++ exercises/small_problems/03.rb
@@ -27,6 +27,16 @@ def transpose(matrix)
   matrix[0].zip(*matrix[1..])
 end
 
+# Further exploration
+
+def transpose!(matrix)
+  0.upto(matrix.size - 1) do |row_i|
+    row_i.upto(matrix.size - 1) do |col_i|
+      matrix[row_i][col_i], matrix[col_i][row_i] = matrix[col_i][row_i], matrix[row_i][col_i]
+    end
+  end
+end
+
 matrix = [
   [1, 5, 8],
   [4, 7, 2],
@@ -37,3 +47,6 @@ new_matrix = transpose(matrix)
 
 p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
 p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+
+transpose!(matrix)
+p matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
