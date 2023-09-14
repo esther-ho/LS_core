8f152d2166c4546d2c1e39cf5cf4678b0e121b04
Esther Ho <ekyho@ucdavis.edu>
2023-09-14 14:05:15 -0700

    Refactor solution for question 2

--- rb120/lesson_2/02.rb
+++ rb120/lesson_2/02.rb
@@ -1,13 +1,14 @@
 class Person
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name = '')
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ''
   end
 
   def name
-    last_name.empty? ? first_name : first_name + ' ' + last_name
+    "#{first_name} #{last_name}".strip
   end
 end
 
