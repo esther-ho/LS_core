ae282846879749e1e72f358ce51d09349d6229e8
Esther Ho <ekyho@ucdavis.edu>
2023-09-15 17:09:34 -0700

    Add solution to Further exploration problem to Exercise 1

--- exercises/rb120_oop/easy_1/01.rb
+++ exercises/rb120_oop/easy_1/01.rb
@@ -39,3 +39,59 @@ puts banner
 # |  |
 # |  |
 # +--+
+
+# Further exploration
+
+class Banner
+  MAX_WIDTH = 1000
+
+  def initialize(message, width = message.size + 2)
+    @message = message
+    @width = set_width(width)
+  end
+
+  def to_s
+    [horizontal_rule, empty_line, message_line, empty_line, horizontal_rule].join("\n")
+  end
+
+  private
+
+  def horizontal_rule
+    "+#{'-' * width}+"
+  end
+
+  def empty_line
+    "|#{' ' * width}|"
+  end
+
+  def message_line
+    "|#{message.center(width)}|"
+  end
+
+  def invalid_width?(value)
+    !value.nil? && (value < message.size || value > MAX_WIDTH)
+  end
+
+  def invalid_width
+    "The width provided is too small/large. Please provide a valid width."
+  end
+
+  def set_width(width)
+    if width.nil?
+      message.size + 2
+    elsif invalid_width?(width)
+      puts invalid_width
+      message.size + 2
+    else
+      width + 2
+    end
+  end
+
+  attr_reader :message, :width
+end
+
+banner = Banner.new('', 10)
+puts banner
+
+banner = Banner.new('To boldly go where no one has gone before.', 20)
+puts banner
