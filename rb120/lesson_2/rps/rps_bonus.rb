592ab334aefeb83e315e2d2ce4af4e0073f0a93c
Esther Ho <ekyho@ucdavis.edu>
2023-09-19 13:40:01 -0700

    Add name and move features to Human class, and Promptable module
    Add features to prompt for name and choice in Promptable module

--- rb120/lesson_2/rps/rps_bonus.rb
+++ rb120/lesson_2/rps/rps_bonus.rb
@@ -2,6 +2,36 @@ require 'psych'
 
 CONFIG = Psych.load_file('config_rpsls.yml')
 
+module Promptable
+  private
+
+  def prompt_name
+    name = nil
+
+    loop do
+      puts "What's your name?"
+      name = gets.chomp.strip
+      break if name =~ /^[a-z]+ *[a-z]+$/i
+      puts "Sorry, please enter a valid name."
+    end
+
+    name.split.map(&:capitalize).join(' ')
+  end
+
+  def prompt_choice
+    choice = nil
+
+    loop do
+      puts "Please choose [r]ock, [p]aper, [sc]issors, [l]izard, or [sp]ock."
+      choice = gets.chomp.strip.downcase
+      break if Move.valid?(choice)
+      puts "Sorry, invalid choice."
+    end
+
+    Move.find(choice)
+  end
+end
+
 class Move
   VALUES = CONFIG['choices']
 
@@ -13,9 +43,20 @@ class Move
     VALUES[value]['beats'].include?(other_move.value)
   end
 
+  def self.valid?(choice)
+    valid_values = VALUES.map { |_, v| v.values_at('inputs') }.flatten
+
+    valid_values.include?(choice)
+  end
+
+  def self.find(abbreviation)
+    VALUES.select { |_, v| v['inputs'].include?(abbreviation) }.keys.first
+  end
+
   protected
   
   attr_reader :value
+
 end
 
 class Player
@@ -40,6 +81,25 @@ class Player
   private
 
   attr_accessor :name, :move_history, :score
+
+  def update_moves(move)
+    move_history << move
+  end
+end
+
+class Human < Player
+  include Promptable
+
+  def choose
+    move = prompt_choice
+    update_moves(move)
+  end
+
+  private
+
+  def set_name
+    self.name = prompt_name
+  end
 end
 
 class Computer < Player
@@ -47,10 +107,6 @@ class Computer < Player
 
   def set_name
     self.name = self.class.to_s
-  end
-
-  def update_moves(move)
-    move_history << move
   end
 end
 
