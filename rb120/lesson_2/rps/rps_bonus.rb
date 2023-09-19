876fd590c83aaf3c673891fa6e9b5e55abecbbc5
Esther Ho <ekyho@ucdavis.edu>
2023-09-19 12:00:17 -0700

    Add name, move, and score features to the Player class and subclasses
    Add features to set name, choose moves, update move history, and update score for Player class as well as its subclass Computer and its children.

--- rb120/lesson_2/rps/rps_bonus.rb
+++ rb120/lesson_2/rps/rps_bonus.rb
@@ -17,3 +17,60 @@ class Move
   
   attr_reader :value
 end
+
+class Player
+  def initialize
+    set_name
+    @move_history = []
+    @score = 0
+  end
+
+  def move
+    move_history.last
+  end
+
+  def update_score
+    self.score += 1
+  end
+
+  def reset_score
+    self.score = 0
+  end
+
+  private
+
+  attr_accessor :name, :move_history, :score
+end
+
+class Computer < Player
+  private
+
+  def set_name
+    self.name = self.class.to_s
+  end
+
+  def update_moves(move)
+    move_history << move
+  end
+end
+
+class Spongebob < Computer
+  def choose
+    move = Move.new(Move::VALUES.keys.sample)
+    update_moves(move)
+  end
+end
+
+class Patrick < Computer
+  def choose
+    move = Move.new('paper')
+    update_moves(move)
+  end
+end
+
+class Squidward < Computer
+  def choose
+    move = Move.new(['rock', 'paper'].sample)
+    update_moves(move)
+  end
+end
