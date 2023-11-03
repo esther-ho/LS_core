33c9944c79c4a0ad5631feac6b76e039ec9cd6a6
Esther Ho <ekyho@ucdavis.edu>
2023-11-03 14:50:55 -0700

    Add solution to Exercise 1

---
+++ exercises/rb120_oop/debugging/01.rb
@@ -0,0 +1,51 @@
+class Library
+  attr_accessor :address, :phone, :books
+
+  def initialize(address, phone)
+    @address = address
+    @phone = phone
+    @books = []
+  end
+
+  def check_in(book)
+    books.push(book)
+  end
+end
+
+class Book
+  attr_accessor :title, :author, :isbn
+
+  def initialize(title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
+  end
+
+  def display_data
+    puts "---------------"
+    puts "Title: #{title}"
+    puts "Author: #{author}"
+    puts "ISBN: #{isbn}"
+    puts "---------------"
+  end
+end
+
+community_library = Library.new('123 Main St.', '555-232-5652')
+learn_to_program = Book.new('Learn to Program', 'Chris Pine', '978-1934356364')
+little_women = Book.new('Little Women', 'Louisa May Alcott', '978-1420951080')
+wrinkle_in_time = Book.new('A Wrinkle in Time', 'Madeleine L\'Engle', '978-0312367541')
+
+community_library.check_in(learn_to_program)
+community_library.check_in(little_women)
+community_library.check_in(wrinkle_in_time)
+
+# Invoking `#books` returns an array of book objects, instead of a single book object.
+# Therefore, invoking `#display_data` on the returned array raises a `NoMethodError`.
+
+# Incorrect code
+# community_library.books.display_data
+
+# Updated code
+community_library.books.each { |book| book.display_data }
+
+# Alternatively, the code in the `Library` class can be altered to include a method to display books
