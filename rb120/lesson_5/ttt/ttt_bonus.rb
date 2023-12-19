d37f6102800979d3e4825eb6e2c22456ba746f79
Esther Ho <ekyho@ucdavis.edu>
2023-12-19 10:58:26 -0800

    Assign markers to players

--- rb120/lesson_5/ttt/ttt_bonus.rb
+++ rb120/lesson_5/ttt/ttt_bonus.rb
@@ -1,6 +1,27 @@
 require 'psych'
+require 'pry'
 
 CONFIG = Psych.load_file('config_ttt.yml')
+
+module Displayable
+  def self.welcome
+    puts "Welcome to Tic Tac Toe!\n"
+  end
+
+  def self.choose_marker
+    puts "Choose a marker:\n"
+  end
+
+  def self.invalid(type)
+    messages = {
+      choice: "Sorry, that's not a valid choice.",
+      name: "Sorry, please enter a valid name.",
+      yes_no: "Sorry, please enter [y]es or [n]o."
+    }
+
+    puts messages[type]
+  end
+end
 
 class Board
   attr_reader :squares
@@ -27,7 +48,15 @@ class Square
   end
 end
 
+class Player
+  def initialize(marker)
+    @marker = marker
+  end
+end
+
 class TTTGame
+  MARKERS = ['O', 'X']
+
   attr_reader :board
 
   def initialize
@@ -35,7 +64,37 @@ class TTTGame
   end
 
   def play
+    Displayable.welcome
+    set_players
     board.draw
+  end
+
+  private
+
+  attr_reader :human, :computer
+
+  def set_players
+    human_choice = valid_marker
+    computer_choice = (MARKERS - [human_choice])[0]
+    @human = Player.new(human_choice)
+    @computer = Player.new(computer_choice)
+  end
+
+  def valid_marker
+    Displayable.choose_marker
+    count = MARKERS.count
+    (1..count).to_a.zip(MARKERS).each do |num, marker|
+      puts "[#{num}] #{marker}"
+    end
+
+    choice = nil
+    loop do
+      choice = gets.chomp
+      break if choice =~ /^\d$/ && (1..count).include?(choice.to_i)
+      Displayable.invalid(:choice)
+    end
+
+    MARKERS[choice.to_i - 1]
   end
 end
 
