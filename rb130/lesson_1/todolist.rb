a9a9ea766adfbd8185994870933db6bd95f402d9
Esther Ho <ekyho@ucdavis.edu>
2024-02-19 15:50:14 -0800

    Uncomment lines

--- rb130/lesson_1/todolist.rb
+++ rb130/lesson_1/todolist.rb
@@ -159,7 +159,7 @@ list = TodoList.new("Today's Todos")
 list.add(todo1)                 # adds todo1 to end of list, returns list
 list.add(todo2)                 # adds todo2 to end of list, returns list
 list.add(todo3)                 # adds todo3 to end of list, returns list
-# list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+list.add(1)                     # raises TypeError with message "Can only add Todo objects"
 
 # <<
 # same behavior as add
@@ -181,45 +181,45 @@ list.to_a                      # returns an array of all items in the list
 #done?
 list.done?                     # returns true if all todos in the list are done, otherwise false
 
-# # # ---- Retrieving an item in the list ----
+# ---- Retrieving an item in the list ----
 
-# # # item_at
-# # list.item_at                    # raises ArgumentError
-# # list.item_at(1)                 # returns 2nd item in list (zero based index)
-# # list.item_at(100)               # raises IndexError
+# item_at
+list.item_at                    # raises ArgumentError
+list.item_at(1)                 # returns 2nd item in list (zero based index)
+list.item_at(100)               # raises IndexError
 
-# # # ---- Marking items in the list -----
+# ---- Marking items in the list -----
 
-# # # mark_done_at
-# # list.mark_done_at               # raises ArgumentError
-# # list.mark_done_at(1)            # marks the 2nd item as done
-# # list.mark_done_at(100)          # raises IndexError
+# mark_done_at
+list.mark_done_at               # raises ArgumentError
+list.mark_done_at(1)            # marks the 2nd item as done
+list.mark_done_at(100)          # raises IndexError
 
-# # # mark_undone_at
-# # list.mark_undone_at             # raises ArgumentError
-# # list.mark_undone_at(1)          # marks the 2nd item as not done,
-# # list.mark_undone_at(100)        # raises IndexError
+# mark_undone_at
+list.mark_undone_at             # raises ArgumentError
+list.mark_undone_at(1)          # marks the 2nd item as not done,
+list.mark_undone_at(100)        # raises IndexError
 
-# # # done!
-# # list.done!                      # marks all items as done
+# done!
+list.done!                      # marks all items as done
 
-# # # ---- Deleting from the list -----
+# ---- Deleting from the list -----
 
-# # # shift
-# # list.shift                      # removes and returns the first item in list
+# shift
+list.shift                      # removes and returns the first item in list
 
-# # # pop
-# # list.pop                        # removes and returns the last item in list
+# pop
+list.pop                        # removes and returns the last item in list
 
-# # # remove_at
-# # list.remove_at                  # raises ArgumentError
-# # list.remove_at(1)               # removes and returns the 2nd item
-# # list.remove_at(100)             # raises IndexError
+# remove_at
+list.remove_at                  # raises ArgumentError
+list.remove_at(1)               # removes and returns the 2nd item
+list.remove_at(100)             # raises IndexError
 
-# # ---- Outputting the list -----
+# ---- Outputting the list -----
 
-# # to_s
-# puts list.to_s                      # returns string representation of the list
+# to_s
+puts list.to_s                      # returns string representation of the list
 
 # ---- Today's Todos ----
 # [ ] Buy milk
