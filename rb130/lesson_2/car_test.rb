ff4d6a94fd170106da89b8595a9c5b080f92e338
Esther Ho <ekyho@ucdavis.edu>
2024-02-20 10:33:29 -0800

    Add custom #== method and test

--- rb130/lesson_2/car_test.rb
+++ rb130/lesson_2/car_test.rb
@@ -36,4 +36,14 @@ class CarTest < Minitest::Test
     arr << @car
     assert_includes(arr, @car)
   end
+
+  def test_value_equality
+    car1 = Car.new
+    car2 = Car.new
+
+    car1.name = "Kim"
+    car2.name = "Kim"
+
+    assert_equal(car1, car2)
+  end
 end
