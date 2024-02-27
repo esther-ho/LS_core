0a73ce2a5e269c529a0c1bf04c5e3b08f08aaed9
Esther Ho <ekyho@ucdavis.edu>
2024-02-27 11:56:40 -0800

    Update solution to Exercise 2 based on further exploration

--- exercises/rb130/02.rb
+++ exercises/rb130/02.rb
@@ -9,3 +9,14 @@ end
 puts compute { 5 + 3 } == 8
 puts compute { 'a' + 'b' } == 'ab'
 puts compute == 'Does not compute.'
+
+# Further exploration
+
+def compute(x)
+  return "Does not compute." unless block_given?
+  yield(x)
+end
+
+puts compute(4) { |n| n + 5 + 3 } == 12
+puts compute('cd') { |s| 'a' + 'b' + s } == 'abcd'
+puts compute(2) == 'Does not compute.'
