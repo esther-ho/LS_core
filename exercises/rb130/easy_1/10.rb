cf296ea84a426001b473a11182169d6865cc275d
Esther Ho <ekyho@ucdavis.edu>
2024-02-29 17:28:05 -0800

    Add solution to Exercise 10

---
+++ exercises/rb130/easy_1/10.rb
@@ -0,0 +1,27 @@
+def count(collection)
+  counter = 0
+  collection.each { |item| counter += 1 if yield(item) }
+  counter
+end
+
+puts count([1,2,3,4,5]) { |value| value.odd? } == 3
+puts count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+puts count([1,2,3,4,5]) { |value| true } == 5
+puts count([1,2,3,4,5]) { |value| false } == 0
+puts count([]) { |value| value.even? } == 0
+puts count(%w(Four score and seven)) { |value| value.size == 5 } == 2
+
+# `count` without `each`, `loop`, `while`, or `until`
+
+def recursive_count(collection)
+  return 0 if collection.empty?
+  count = (yield(collection[-1]) ? 1 : 0)
+  count + recursive_count(collection[0..-2])
+end
+
+puts count([1,2,3,4,5]) { |value| value.odd? } == 3
+puts count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+puts count([1,2,3,4,5]) { |value| true } == 5
+puts count([1,2,3,4,5]) { |value| false } == 0
+puts count([]) { |value| value.even? } == 0
+puts count(%w(Four score and seven)) { |value| value.size == 5 } == 2
