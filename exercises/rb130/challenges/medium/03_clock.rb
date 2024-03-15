147d12e2b8479723ad92612a3a1780845e55701e
Esther Ho <esther.ho.ucd@gmail.com>
2024-03-15 11:41:29 -0700

    Remove ref to Clock class within class def, and update #- method

--- exercises/rb130/challenges/medium/03_clock.rb
+++ exercises/rb130/challenges/medium/03_clock.rb
@@ -45,7 +45,7 @@ class Clock
   MIN_PER_DAY = MIN_PER_HOUR * 24
 
   def self.at(hour, min = 0)
-    Clock.new(hour * MIN_PER_HOUR + min)
+    new(hour * MIN_PER_HOUR + min)
   end
 
   def initialize(total_min)
@@ -54,12 +54,11 @@ class Clock
   
   def +(min)
     new_total_min = (@total_min + min) % MIN_PER_DAY
-    Clock.new(new_total_min)
+    self.class.new(new_total_min)
   end
   
   def -(min)
-    new_total_min = (@total_min - min) % MIN_PER_DAY
-    Clock.new(new_total_min)
+    self + (-min)
   end
   
   def ==(other)
