4daa7ae85dd69cc475e923dd556f6c36d06c5b7b
Esther Ho <esther.ho.ucd@gmail.com>
2024-08-07 14:31:19 -0700

    Complete 'Create and View Tables' exercises

---
+++ ls180/sql_book/ls_burger2.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE orders (
+  id serial,
+  customer_name varchar(100) NOT NULL,
+  burger varchar(50),
+  side varchar(50),
+  drink varchar(50),
+  order_total decimal(4,2) NOT NULL
+);
