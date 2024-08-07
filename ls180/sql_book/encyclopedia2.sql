96a782e9562ddb7d503461d9ee310ea535007d33
Esther Ho <esther.ho.ucd@gmail.com>
2024-08-07 15:03:06 -0700

    Complete 'Add data with INSERT' exercises

--- ls180/sql_book/encyclopedia2.sql
+++ ls180/sql_book/encyclopedia2.sql
@@ -27,3 +27,41 @@ ALTER TABLE animals
 ALTER TABLE animals
   ADD UNIQUE (binomial_name);
 
+-- Add data to `countries` table
+INSERT INTO countries (name, capital, population)
+VALUES ('France', 'Paris', 67158000),
+       ('USA', 'Washington D.C.', 325365189),
+       ('Germany', 'Berlin', 82349400),
+       ('Japan', 'Tokyo', 126672000);
+
+-- Add data to `celebrities` table
+INSERT INTO celebrities (first_name, last_name, occupation, date_of_birth, deceased)
+VALUES ('Bruce', 'Springsteen', 'Singer, Songwriter', '1949-09-23', false),
+       ('Scarlett', 'Johansson', 'Actress', '1984-11-22', DEFAULT),
+       ('Frank', 'Sinatra', 'Singer, Actor', '1915-12-12', true),
+       ('Tom', 'Cruise', 'Actor', '1962-07-03', DEFAULT);
+
+-- DROP `NOT NULL` constraint from `last_name` column
+ALTER TABLE celebrities
+  ALTER COLUMN last_name DROP NOT NULL;
+
+-- Add data to `celebrities` table for celebrities with no last name
+INSERT INTO celebrities (first_name, occupation, date_of_birth, deceased)
+VALUES ('Madonna', 'Singer, Actress', '1958-08-16', false),
+       ('Prince', 'Singer, Songwriter, Musician, Actor', '1958-06-07', true);
+
+-- Add data to `celebrities` table with `NULL` value in `deceased` column
+INSERT INTO celebrities (first_name, last_name, occupation, date_of_birth, deceased)
+VALUES ('Elvis', 'Presley', 'Singer, Musician, Actor', '1935-01-08', NULL);
+
+-- Drop `UNIQUE` constraint from `binomial_name` column in `animals` table
+ALTER TABLE animals
+  DROP CONSTRAINT animals_binomial_name_key;
+
+-- Insert data into `animals` table
+INSERT INTO animals (name, binomial_name, max_weight_kg, max_age_years, conservation_status)
+VALUES ('Dove', 'Columbidae Columbiformes', 2, 15, 'LC'),
+       ('Golden Eagle', 'Aquila Chrysaetos', 6.35, 24, 'LC'),
+       ('Peregrine Falcon', 'Falco Peregrinus', 1.5, 15, 'LC'),
+       ('Pigeon', 'Columbidae Columbiformes', 2, 15, 'LC'),
+       ('Kakapo', 'Strigops habroptila', 4, 60, 'CR');
--- ls180/sql_book/ls_burger3.sql
+++ ls180/sql_book/ls_burger3.sql
@@ -19,3 +19,14 @@ ALTER TABLE orders
 ALTER TABLE orders
   DROP COLUMN order_total;
 
+-- Insert data into `orders` table
+INSERT INTO orders (customer_name, customer_email, customer_loyalty_points,
+                    burger, side, drink, burger_cost, side_cost, drink_cost)
+VALUES ('James Bergman', 'james1998@email.com', (20 + 3 + 5),
+        'LS Chicken Burger','Fries', 'Cola', 4.50, 0.99, 1.50),
+       ('Natasha O''Shea', 'natasha@osheafamily.com', (15 + 3),
+        'LS Cheeseburger', 'Fries', NULL, 3.50, 0.99, DEFAULT),
+       ('Natasha O''Shea', 'natasha@osheafamily.com', (30 + 5 + 7),
+        'LS Double Deluxe Burger', 'Onion Rings', 'Chocolate Shake', 6.00, 1.50, 2.00),
+       ('Aaron Muller', NULL, 10,
+        'LS Burger', NULL, NULL, 3.00, DEFAULT, DEFAULT);
