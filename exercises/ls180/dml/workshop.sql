ef290ce80c37c7a7b3d0e8b31956f45ffb9cb525
Esther Ho <esther.ho.ucd@gmail.com>
2024-08-09 16:07:02 -0700

    Set up the `workshop` database
    - Create and connect to the `workshop` database
    - Create the `devices` and `parts` tables

---
+++ exercises/ls180/dml/workshop.sql
@@ -0,0 +1,34 @@
+-- Create a `workshop` database
+CREATE DATABASE workshop;
+
+-- Connect to `workshop` database
+\c
+
+-- Delete tables if they exist
+DROP TABLE IF EXISTS devices, parts;
+
+/*
+- Create a `devices` table with:
+  - `id` column that is auto-incrementing and a primary key
+  - `name` column that can contain a string and is required
+  - `created_at` column that lists the `timestamp` the device was created and should have a default value
+*/
+CREATE TABLE devices (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  created_at timestamp DEFAULT NOW()
+);
+
+/*
+- Create a `parts` table with:
+  - `id` column that is auto-incrementing and a primary key
+  - `part_number` column that contains unique and not-null values
+  - `device_id` column that is a foreign key referencing `devices.id`
+- Each device should have many different parts
+- Each part is unique to each device
+*/
+CREATE TABLE parts (
+  id serial PRIMARY KEY,
+  part_number integer UNIQUE NOT NULL,
+  device_id integer REFERENCES devices(id)
+);
