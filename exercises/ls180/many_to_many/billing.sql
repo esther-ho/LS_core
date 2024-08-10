638346534fab2b14456d5ab84bbdb4437d4bad33
Esther Ho <esther.ho.ucd@gmail.com>
2024-08-09 20:49:38 -0700

    Add join table to `billing` database
    - Create a `customers_services` join table
    - Add data to the join table to associate customer and service records

--- exercises/ls180/many_to_many/billing.sql
+++ exercises/ls180/many_to_many/billing.sql
@@ -55,3 +55,28 @@ VALUES ('Unix Hosting', 5.95),
 
 SELECT * FROM customers;
 SELECT * FROM services;
+
+/*
+- Create a join table that associates customers with services and vice-versa
+- It should have a primary key `id` that auto-increments
+- Deleting the customer record will automatically delete corresponding records in the join table
+- Each customer-service combination should be unique
+*/
+CREATE TABLE customers_services (
+  id serial PRIMARY KEY,
+  customer_id integer NOT NULL,
+  service_id integer NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(id)
+  ON DELETE CASCADE,
+  UNIQUE (customer_id, service_id)
+);
+
+-- Insert data into join table
+INSERT INTO customers_services (customer_id, service_id)
+VALUES (1, 1), (1, 2), (1, 3),
+       (3, 1), (3, 2), (3, 3), (3, 4), (3, 5),
+       (4, 1), (4, 4),
+       (5, 1), (5, 2), (5, 6),
+       (6, 1), (6, 6), (6, 7);
+
+SELECT * FROM customers_services;
