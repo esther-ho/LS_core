48c5f83c999027857fd4b12cd3356bdd7a5e822c
Esther Ho <esther.ho.ucd@gmail.com>
2024-08-28 11:21:45 -0700

    Separate HTTP request line into different components
    - Split request line into HTTP method, path, parameters, and HTTP version
    - Convert parameters strings into a hash
    - Print each component in the web browser

--- rb175/echo_server/server.rb
+++ rb175/echo_server/server.rb
@@ -8,6 +8,17 @@ loop do
   next if !request_line || request_line =~ /favicon/
   puts request_line
 
+  
+  http_method, path_and_params, http_version = request_line.split
+  path, params = path_and_params.split('?')
+  params = (params || '').split('&')
+  params = params.map { |pair| pair.split('=') }.to_h
+  
   client.puts request_line
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts http_version
+
   client.close
 end
