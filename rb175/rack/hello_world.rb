1edef78d6c4ae8415ebc0aab62b820fece99c20b
Esther Ho <esther.ho.ucd@gmail.com>
2024-08-30 11:30:21 -0700

    Add routing to the HelloWorld application
    - Create an `advice.rb` file that generates a random string of advice
    - Display the random string of advice when the `REQUEST_PATH` is `/advice`
    - Display `'404 Not Found'` if the `REQUEST_PATH` is neither the root path or `/advice`

--- rb175/rack/hello_world.rb
+++ rb175/rack/hello_world.rb
@@ -1,5 +1,15 @@
+require_relative 'advice'
+
 class HelloWorld
   def call(env)
-    ['200', {'Content-Type' => 'text/plain'}, ['Hello World!']]
+    case env['REQUEST_PATH']
+    when "/"
+      ['200', {'Content-Type' => 'text/plain'}, ['Hello World!']]
+    when "/advice"
+      piece_of_advice = Advice.new.generate
+      ['200', {'Content-Type' => 'text/plain'}, [piece_of_advice]]
+    else
+      ['404', {'Content-Type' => 'text/plain', 'Content-Length' => '13'}, ['404 Not Found']]
+    end
   end
 end
