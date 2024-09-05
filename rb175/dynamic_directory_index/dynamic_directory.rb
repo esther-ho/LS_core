4e161318a229565338fa6e588e7c88f3e98324dc
Esther Ho <esther.ho.ucd@gmail.com>
2024-09-05 11:37:56 -0700

    Add sort feature
    - Sort the list of files in ascending order by default
    - Sort the list in descending order if the query parameter 'sort' is 'desc'

--- rb175/dynamic_directory_index/dynamic_directory.rb
+++ rb175/dynamic_directory_index/dynamic_directory.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get "/" do
-  @files = Dir.children("public")
+  @files = Dir.children("public").sort
+  @files.reverse! if params['sort'] == 'desc'
+
   erb :index
 end
