ddc0201fd8225d5d3e017e613cabfe30a83d0bc8
Esther Ho <esther.ho.ucd@gmail.com>
2024-09-10 17:29:01 -0700

    Create a page for each user
    - Create a route for each user
    - Redirect to the 'users' page if the user navigates to a user's pag
    e does not exist
    - Display the user's email address on each page
    - Display the user's interests as a comma-delimited string

--- rb175/users_and_interests/users_and_interests.rb
+++ rb175/users_and_interests/users_and_interests.rb
@@ -11,7 +11,17 @@ get "/" do
   redirect "/users"
 end
 
+get "/users/:name" do
+  name = params[:name].to_sym
+  redirect "/users" unless @user_data.key?(name)
+  @email = @user_data[name][:email]
+  @interests = @user_data[name][:interests]
+
+  erb :user
+end
+
 get "/users" do
   @users = @user_data.keys
   erb :users
 end
+
