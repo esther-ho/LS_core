faa5faceaa888019cd66a47eb218a1385285afd6
Esther Ho <esther.ho.ucd@gmail.com>
2024-09-10 17:56:58 -0700

    Add the user and interest count at the bottom of every page
    - Create the 'count_interests' view helper to return the sum of interests
    - Add a paragraph in the layout file to display the total number of users and interests

--- rb175/users_and_interests/users_and_interests.rb
+++ rb175/users_and_interests/users_and_interests.rb
@@ -3,6 +3,14 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require "psych"
 
+helpers do
+  def count_interests
+    @user_data.values.reduce(0) do |sum, user_info|
+      sum + user_info[:interests].size
+    end
+  end
+end
+
 before do
   @user_data = Psych.load_file "data/users.yml"
   @users = @user_data.keys
