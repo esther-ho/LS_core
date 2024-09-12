0baf87a4eb9cf8ddc7e5743ef15b1ff9eaef7a19
Esther Ho <esther.ho.ucd@gmail.com>
2024-09-12 10:30:12 -0700

    Configure a session
    - Define a configure block that creates a session hash
    - Generate a random session secret
    - Store and reference the list data from the session
    - Use a before filter to ensure that the list data in the session is at least an empty array

--- rb175/todos/todo.rb
+++ rb175/todos/todo.rb
@@ -2,14 +2,20 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    { name: "Lunch Groceries", todos: [] },
-    { name: "Dinner Groceries", todos: [] }
-  ]
+  @lists = session[:lists]
   erb :lists, layout: :layout
 end
