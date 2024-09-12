88233f4314b7a5764e6e1344aa9674d295c98998
Esther Ho <esther.ho.ucd@gmail.com>
2024-09-12 11:27:56 -0700

    Add comments to indicate the function of each route

--- rb175/todos/todo.rb
+++ rb175/todos/todo.rb
@@ -15,15 +15,18 @@ get "/" do
   redirect "/lists"
 end
 
+# View list of lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
   session[:lists] << { name: params[:list_name], todos: [] }
   redirect "/lists"
