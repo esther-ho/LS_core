e9d41579f29826f0e7e29931708cba4be7b451df
Esther Ho <esther.ho.ucd@gmail.com>
2024-10-21 17:58:23 -0700

    Add feature to mark todos as completed or uncompleted
    - Add a form in front of the todo item name to mark a todo as completed or uncompleted
    - Update the value of the `:completed` key based on the value returned by the form
    - Style completed items using the "complete" class attribute

--- rb175/todos/todo.rb
+++ rb175/todos/todo.rb
@@ -128,3 +128,17 @@ post "/lists/:list_id/todos/:id/delete" do
 
   redirect "/lists/#{list_id}"
 end
+
+# Update the status of a todo
+post "/lists/:list_id/todos/:id" do
+  list_id = params[:list_id].to_i
+  list = session[:lists][list_id]
+  todo_id = params[:id].to_i
+  todo = list[:todos][todo_id]
+
+  is_completed = (params[:completed] == "true")
+  todo[:completed] = is_completed
+
+  session[:success] = "The todo has been updated."
+  redirect "/lists/#{list_id}"
+end
