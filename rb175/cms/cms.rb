dc4e1f05eae835a50b973a2883ebe8f5228ef37e
Esther Ho <esther.ho.ucd@gmail.com>
2024-10-28 16:25:54 -0700

    Add an index page
    - Retrieve the absolute path of the project directory
    - Display the names of all files present in the 'data' directory

--- rb175/cms/cms.rb
+++ rb175/cms/cms.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+root = File.expand_path('..', __FILE__)
+
+# Display the index page
 get "/" do
-  "Getting started."
+  @files = Dir.glob("#{root}/data/*").map do |file|
+    File.basename(file)
+  end
+
+  erb :index
 end
