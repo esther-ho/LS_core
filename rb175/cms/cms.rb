d975bda730b75d6f42c2bd0e8f258915d9620329
Esther Ho <esther.ho.ucd@gmail.com>
2024-10-29 11:02:55 -0700

    Display each file in plain text
    - Add links to each list element in the index page
    - Add new route to display each file in plain text

--- rb175/cms/cms.rb
+++ rb175/cms/cms.rb
@@ -12,3 +12,10 @@ get "/" do
 
   erb :index
 end
+
+# Display each file in plain text
+get "/:filename" do
+  file_path = "#{root}/data/#{params[:filename]}"
+  headers['Content-Type'] = 'text/plain'
+  File.read(file_path)
+end
